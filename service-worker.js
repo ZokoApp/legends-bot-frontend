b1dd0399e8642de2f415039adc7af5659a6326ca
ZokoApp <softwareandapp.arg@gmail.com>
2026-01-29 19:39:10 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,39 +5,3 @@ self.addEventListener("install", e => {
 self.addEventListener("fetch", e => {
   // base vacía por ahora
 });
-
-
-// =======================
-// PUSH RECEIVER
-// =======================
-self.addEventListener("push", event => {
-  if (!event.data) return;
-
-  const data = event.data.json();
-
-  const title = data.title || "Legends Bot";
-  const options = {
-    body: data.body || "Nueva notificación",
-    icon: data.icon || "/icons/icon-192.png",
-    badge: data.badge || "/icons/icon-192.png",
-    data: data.url || "/"
-  };
-
-  event.waitUntil(
-    self.registration.showNotification(title, options)
-  );
-});
-
-self.addEventListener("notificationclick", event => {
-  event.notification.close();
-
-  event.waitUntil(
-    clients.matchAll({ type: "window", includeUncontrolled: true }).then(list => {
-      if (list.length > 0) {
-        list[0].focus();
-      } else {
-        clients.openWindow("/");
-      }
-    })
-  );
-});
